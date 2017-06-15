be1ced37d9b18efa4992341d86bc5d6c28be5d9b
Bryan Anderson <bryan.anderson@mobiwm.com>
2017-06-15 12:36:34 -0400

    Finish static pages

--- test/helpers/test_helpers.rb
+++ test/helpers/test_helpers.rb
@@ -1,12 +1,14 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require "minitest-reporters"
-Minitest::Reporters.use!
+require 'minitest/rails'
+require "minitest/reporters"
+reporter_options = {color: true, slow_count: 5}
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new(:color => true, :slow_count => 5)]
 
 class ActiveSupport::TestCase
   # Setup all fixtures in text/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...  
+  # Add more helper methods to be used by all tests here...
 end
