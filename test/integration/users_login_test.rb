8ff2af9bc9e731bd9f6f5ea6d8c95b873dfbd290
Bryan Anderson <bryan.anderson@mobiwm.com>
2017-06-16 15:34:33 -0400

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,7 +30,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: {session:{email:@user.email,password:'password'}}
-    # assert is_logged_in? # integration test is not picking up the test help (is_logged_in?)
+    assert !session[:user_id].nil?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -38,12 +38,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
-    # assert_not is_logged_in? # integration test is not picking up the test help (is_logged_in?)
+    assert_not !session[:user_id].nil?
     assert_redirected_to root_url
+    # Simulate a user clicking Logout in a second window
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count:0
     assert_select "a[href=?]", user_path(@user), count:0
+  end
+
+  test "login with remembering" do
+    post login_path, params:{session: {email:@user.email,password:'password',remember_me: '1'}}
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # log in to set the cookie.
+    post login_path, params:{session: {email:@user.email,password:'password',remember_me: '1'}}
+    # login again and verify that the cookie is deleted.
+    post login_path, params:{session: {email:@user.email,password:'password',remember_me: '0'}}
+    assert_empty cookies['remember_token']
   end
 
 end
